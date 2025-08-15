ca1944849843481180c86a56106c70ea879a2564
KestroyStephan <stephankestroy@gmail.com>
2025-08-15 12:24:46 +0530

    Adden Header section

---
+++ app/_components/Header.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+const menuOptions=[
+  {
+    name:'Home',
+    path:'/'
+  },
+  {
+    name:'Pricing',
+    path:'/pricing'
+  },
+  {
+    name:'Contact us',
+    path:'/contact-us'
+  }
+]
+
+function Header() {
+  return (
+    <div>
+      {/*Logo*/}
+      <div className='flex gap-2 items-center'>
+        <Image src={'/logo.svg'} alt="logo" width={30} height={30}/>
+        <h2 className='font-bold text-2xl'>AI Based Trip Planner</h2>
+      </div>
+       {/*Menu Options*/}
+       <div className='flex gap-8 items-center'>
+        {menuOptions.map((menu, index) => (
+          <Link href={menu.path}>
+            <h2 className='text-lg hover:scale-105 transition-all'>{menu.name}</h2>
+          </Link>
+        ))}
+       </div>
+        {/*Get Starded Button*/}
+    </div>
+  )
+}
+
+export default Header
