633484b4f992a12dbc733f74de51e2ecfb50b8c6
Kestroy <161330387+KestroyStephan@users.noreply.github.com>
2025-09-16 14:33:30 +0530

    hero section created

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
@@ -5,7 +6,7 @@ import React from 'react'
 const menuOptions=[
   {
     name:'Home',
-    path:'/'
+    path:'/home'
   },
   {
     name:'Pricing',
@@ -19,7 +20,7 @@ const menuOptions=[
 
 function Header() {
   return (
-    <div>
+    <div className='flex justify-between items-center p-4'>
       {/*Logo*/}
       <div className='flex gap-2 items-center'>
         <Image src={'/logo.svg'} alt="logo" width={30} height={30}/>
@@ -29,11 +30,12 @@ function Header() {
        <div className='flex gap-8 items-center'>
         {menuOptions.map((menu, index) => (
           <Link href={menu.path}>
-            <h2 className='text-lg hover:scale-105 transition-all'>{menu.name}</h2>
+            <h2 className='text-lg hover:scale-105 transition-all hover:text-primary'>{menu.name}</h2>
           </Link>
         ))}
        </div>
         {/*Get Starded Button*/}
+        <Button>Get Started</Button>
     </div>
   )
 }
